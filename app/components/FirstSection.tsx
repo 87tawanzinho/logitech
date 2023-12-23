1c477ca4ce124dd52ff93bd6420237a3123bbabc
tawan <87tawanzinho@gmail.com>
2023-12-23 12:19:54 -0300

    wow

--- app/components/FirstSection.tsx
+++ app/components/FirstSection.tsx
@@ -1,20 +1,30 @@
+import Image from "next/image";
+import mainMouse from "@/public/mouse/main-mouse.webp";
 export default function FirstSection() {
   return (
-    <div className="flex justify-between items-center sectionOneComputer text-white mt-20 relative ">
+    <div className="flex justify-between items-center sectionOneComputer text-white mt-20 p-4 relative ">
       <div className="flex flex-col">
         <p className="text-xs">/ Produtos / Mouses Para Jogos / Pro Hero</p>
-        <p>IMAGEM TODO </p>
+        <div>
+          <Image src={mainMouse} alt="mainMouse" />
+        </div>
       </div>
 
-      <div className="flex flex-col w-80 items-start absolute right-0 top-12 ">
+      <div className="flex flex-col w-96 items-start absolute right-0 top-12 ">
         <p className="bg-white text-black px-2 text-xs flex items-center justify-center gap-1">
           <span className="font-bold text-lg">PRO</span> SÉRIES
         </p>
         <h1 className="text-3xl mt-8">PRÓ GAMING MOUSE</h1>
         <p className="text-sm mt-4">Mouse profissional para jogos</p>
-        <button className="w-80 bg-blue-900 p-4 mt-4">
+        <button className="w-80 bg-blue-900 p-4 mt-4 rounded">
           COMPRAR DE LOGI LOJA {">"}
         </button>
+        <p className="mt-10">
+          O mouse PRO Gaming foi criado com um objetivo: ajudar você no ambiente
+          extremamente competitivo e acelerado dos esportes. Aprimorado com o
+          sensor HERO para oferecer a incrível velocidade e precisão que você
+          precisa para vencer.
+        </p>
       </div>
     </div>
   );
